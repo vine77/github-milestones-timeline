cddd94e7f8ed0547039b8a446fc3842242cb4d0d
Nathan Ward <175123+vine77@users.noreply.github.com>
2022-09-21 03:20:44 -0700

    Add current date indicator, refresh on sort, and duplicate date handling

--- github-milestones-timeline.user.js
+++ github-milestones-timeline.user.js
@@ -43,21 +43,35 @@ function addMilestonesStyles() {
       background: #afb8c1;
       height: 2px;
       left: 0;
-      transform: translateY(-50%);
       position: absolute;
       right: 0;
       top: 50%;
+      transform: translateY(-50%);
     }
     .milestones-timeline-milestone {
+      background: white;
+      border-radius: 50%;
+      border: 2px solid #afb8c1;
+      height: 10px;
       position: absolute;
       top: 50%;
       transform: translateY(-50%) translateX(-7px);
-      background: white;
-      height: 10px;
       width: 10px;
-      border-radius: 50%;
-      border: 2px solid #afb8c1;
-
+    }
+    .milestones-timeline-milestone-now {
+      background: #1a7f37;
+      border: transparent;
+      height: 7px;
+      width: 7px;
+      z-index: 1;
+    }
+    .milestones-timeline-milestone-date {
+      font-size: 12px;
+      left: 50%;
+      position: absolute;
+      top: 8px;
+      transform: translateX(-50%);
+      white-space: nowrap;
     }
   `);
 }
@@ -76,30 +90,90 @@ function getMilestones() {
       : dateElement.textContent.trim().replace('Due by ', '');
     return { title, date: new Date(dateText) };
   });
+  milestones.push({
+    date: new Date(),
+    title: 'Now',
+    type: 'now',
+  });
 
   return milestones.sort((a, b) => a.date - b.date);
 }
 
+function getShortDate(date) {
+  return !date
+    ? date
+    : new Intl.DateTimeFormat('en-US', {
+        month: 'short',
+        day: 'numeric',
+      }).format(date);
+}
+
+function getISODate(date) {
+  return !date
+    ? date
+    : date.toISOString().substring(0, date.toISOString().indexOf('T'));
+}
+
+function onHistoryUpdated(callback) {
+  let previousUrl = '';
+  const observer = new MutationObserver(function (mutations) {
+    if (location.href !== previousUrl) {
+      if (previousUrl) callback(location);
+      previousUrl = location.href;
+    }
+  });
+  observer.observe(document, { subtree: true, childList: true });
+}
+onHistoryUpdated(() => setTimeout((location) => addMilestonesTimeline(), 10));
+
 function addMilestonesTimeline() {
   const milestones = getMilestones();
   if (milestones.length < 2) return;
+  if (document.querySelector('.milestones-timeline')) {
+    document.querySelector('.milestones-timeline').remove();
+  }
 
   const tableHeaderElement = document.querySelector('.table-list-header');
   const timelineElement = document.createElement('div');
   timelineElement.classList.add('milestones-timeline');
+
   const lineElement = document.createElement('div');
   lineElement.classList.add('milestones-timeline-line');
   timelineElement.appendChild(lineElement);
-  milestones.forEach((milestone) => {
-    const milestoneElement = document.createElement('div');
-    milestoneElement.classList.add('milestones-timeline-milestone');
-    milestoneElement.style.left = `${
-      ((milestone.date - milestones[0].date) /
-        (milestones[milestones.length - 1].date - milestones[0].date)) *
-      100
-    }%`;
-    // milestoneElement.textContent = milestone.title;
-    timelineElement.appendChild(milestoneElement);
+
+  milestones.forEach((milestone, i) => {
+    const isDuplicateDate =
+      getShortDate(milestones[i - 1]?.date) === getShortDate(milestone.date);
+    let milestoneElement;
+
+    if (isDuplicateDate) {
+      milestoneElement = timelineElement.querySelector(
+        `.milestones-timeline-milestone[datetime="${getISODate(
+          milestone.date
+        )}"]`
+      );
+    } else {
+      // Add dot for milestone
+      milestoneElement = document.createElement('div');
+      milestoneElement.classList.add('milestones-timeline-milestone');
+      milestoneElement.setAttribute('datetime', getISODate(milestone.date));
+      milestoneElement.style.left = `${
+        ((milestone.date - milestones[0].date) /
+          (milestones[milestones.length - 1].date - milestones[0].date)) *
+        100
+      }%`;
+      timelineElement.appendChild(milestoneElement);
+
+      if (milestone.type === 'now') {
+        milestoneElement.classList.add('milestones-timeline-milestone-now');
+      } else {
+        // Add milestone date
+        dateElement = document.createElement('span');
+        dateElement.classList.add('milestones-timeline-milestone-date');
+        dateElement.textContent = getShortDate(milestone.date);
+        milestoneElement.appendChild(dateElement);
+      }
+    }
   });
 
   tableHeaderElement.parentElement.insertBefore(
